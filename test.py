8ed4c984d07ce2c928659646ede0ab58925334b8
Frank <frank.lucassen@unibw.de>
2023-06-08 18:53:16 +0200

    test

--- test.py
+++ test.py
@@ -1,7 +1,29 @@
+import threading
+from threading import Timer
+import time
+import queue
 import random
 
-for i in range(0,5000):
-    print(round(random.random() * 8000 + 16000))
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
 
-for i in range(0,5000):
-    print(round(random.random() * 8000 + 48000))
+q = queue.Queue()
+global t
+t = 0
+
+
+def func():
+    global t
+    q.put(random.random())
+    print(time.time() - t)
+    t = time.time()
+
+
+timer = RepeatTimer(0.001, func)
+timer.start()
+
+while True:
+    t = time.time()
+    time.sleep(5)
